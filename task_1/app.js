d7779b29e15ef01c99a8406ccebe78c9222c73e2
Serge Cage <sergeycage@rocketmail.com>
2024-04-21 22:27:04 +0300

    init: starts project

---
+++ task_1/app.js
@@ -0,0 +1,7 @@
+import {encoded, translations} from './data.js'
+
+const translateIdLibrary = (source, target) => {
+    const result = source.reduce(index, value => element.index == target.value);
+    console.log(result);
+}
+translateIdLibrary(encoded,translations);
